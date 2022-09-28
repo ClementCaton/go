ea8c253e5aa09684e670b8793d263962acc810e9
JasonGROSSO <114397870+JasonGROSSO@users.noreply.github.com>
2022-09-28 10:19:48 +0200

    refactor hello world test

--- greet/main_test.go
+++ greet/main_test.go
@@ -2,11 +2,10 @@ package main
 
 import "testing"
 
+//test printer function
 func TestPrinter(t *testing.T) {
 	var a = printer("Hello World!")
-	if a == "Hello World!" {
-		return
-	} else {
+	if a != "Hello World!" {
 		t.Error("Expected 'Hello World!', Got ", a)
 	}
 }
