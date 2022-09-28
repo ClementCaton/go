679fe3f9431f7d762b76a83ef1fd66794bfdeda6
JasonGROSSO <114397870+JasonGROSSO@users.noreply.github.com>
2022-09-28 11:24:30 +0200

    add Lucien case

--- greet/main_test.go
+++ greet/main_test.go
@@ -4,9 +4,18 @@ import "testing"
 
 //test printer function
 func TestPrinter(t *testing.T) {
-	var a = greeter("Algosup")
-	var want = "Hello Algosup"
-	if a != want {
-		t.Errorf("Expected %q, Got %q", want, a)
-	}
+	t.Run("greet algosup", func(t *testing.T) {
+		var a = greeter("Algosup")
+		var want = "Hello Algosup"
+		if a != want {
+			t.Errorf("Expected %q, Got %q", want, a)
+		}
+	})
+	t.Run("greet Lucien", func(t *testing.T) {
+		var a = greeter("Lucien")
+		var want = "Hello Lucien"
+		if a != want {
+			t.Errorf("Expected %q, Got %q", want, a)
+		}
+	})
 }
