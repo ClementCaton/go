1702a40d91299fde24e2d94e37c48b7234748cef
JasonGROSSO <jason.grosso@algosup.com>
2022-09-28 12:31:15 +0200

    refactor sum test

--- sum/main_test.go
+++ sum/main_test.go
@@ -5,9 +5,18 @@ import "testing"
 func TestSum(t *testing.T) {
 	t.Run("sum", func(t *testing.T) {
 		b := []int{1, 2, 3}
-		var a = sum(b)
-		if a != 6 {
-			t.Errorf("Expected %d, Got %d", 6, a)
+		var got = sum(b)
+		var want int = 6
+		if got != want {
+			t.Errorf("Expected %d, Got %d", want, got)
+		}
+	})
+	t.Run("sum", func(t *testing.T) {
+		b := []int{1, 2, 3, 4, 5}
+		var got = sum(b)
+		var want int = 15
+		if got != want {
+			t.Errorf("Expected %d, Got %d", want, got)
 		}
 	})
 }
