e72f51fef41654bc9aeabeaa7700609bbd88d3bd
JasonGROSSO <114397870+JasonGROSSO@users.noreply.github.com>
2022-09-28 11:32:12 +0200

    We now recognise French language

--- greet/main.go
+++ greet/main.go
@@ -4,10 +4,13 @@ import "os"
 
 func main() {
 	var input string = os.Args[1]
-	var a = greeter(input)
+	var a = greeter(input, "french")
 	println(a)
 }
 
-func greeter(input string) string {
+func greeter(input string, lang string) string {
+	if lang == "french" {
+		return "Bonjour " + input
+	}
 	return "Hello " + input
 }
--- greet/main_test.go
+++ greet/main_test.go
@@ -5,15 +5,22 @@ import "testing"
 //test printer function
 func TestPrinter(t *testing.T) {
 	t.Run("greet algosup", func(t *testing.T) {
-		var a = greeter("Algosup")
+		var a = greeter("Algosup", "english")
 		var want = "Hello Algosup"
 		if a != want {
 			t.Errorf("Expected %q, Got %q", want, a)
 		}
 	})
 	t.Run("greet Lucien", func(t *testing.T) {
-		var a = greeter("Lucien")
+		var a = greeter("Lucien", "english")
 		var want = "Hello Lucien"
+		if a != want {
+			t.Errorf("Expected %q, Got %q", want, a)
+		}
+	})
+	t.Run("greet Lucien in french", func(t *testing.T) {
+		var a = greeter("Lucien", "french")
+		var want = "Bonjour Lucien"
 		if a != want {
 			t.Errorf("Expected %q, Got %q", want, a)
 		}
